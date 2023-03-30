cffe01233b43927f76a11be6d13d236ba19d125b
rm-marzan <129345961+rm-marzan@users.noreply.github.com>
2023-03-30 11:12:36 +0600

    Delete footer.jsx

--- src/component/layout/footer/footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './footer.scss';
-
-const Footer = () => {
-  return <footer className="footer mt-auto bg-dark text-white p-3"> PM Vault © {(new Date().getFullYear())}</footer>;
-};
-
-export default Footer;
